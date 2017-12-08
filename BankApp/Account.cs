d99444622d6b40d305add948043e94f250aff133
Lindsay <lterchin@gmail.com>
2017-12-08 08:05:25 -0800

    changes from class on 12/2

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -6,17 +6,59 @@ using System.Threading.Tasks;
 
 namespace BankApp
 {
+
+    enum TypeOfAccount
+    {
+        Checking,
+        Savings,
+        CD,
+        Loan
+    }
+
+
     /// <summary>
     /// This is about a bank account
     /// </summary>
     class Account
     {
+        #region static
+        private static int lastAccountNumber = 0;
+        #endregion
+
         #region Properties
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; private set; }
         public string EmailAddress { get; set; }
         public string AccountName { get; set; }
-        public string AccountType { get; set; }
-        public decimal Balance { get; set; }
+        public TypeOfAccount AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Constructor (name should be the same as the class name)
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+
+        }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        /// <summary>
+        /// Withdraw money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be withdrawn</param>
+        public void Withdrawal(decimal amount)
+        {
+            Balance -= amount;
+        }
         #endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,6 +10,43 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("**********************");
+            Console.WriteLine("Welcome to the bank!");
+            Console.WriteLine("**********************");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create an account");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Withdraw");
+            Console.WriteLine("4. Print all accounts");
+
+            Console.Write("Please choose one option from above: ");
+            var choice = Console.ReadLine();
+
+            switch(choice)
+
+            {
+                case "0":
+                    Console.WriteLine("Thank you for visiting");
+                    return;
+                case "1":
+                    Console.Write("Please provide your email address:");
+                    var emailAddress = Console.ReadLine();
+                    Console.Write("PLease provide your account name:");
+                    var accountName = Console.ReadLine();
+                    Console.WriteLine("Please choose your account type");
+                    var typeOfAccounts = Enum.GetNames(typeof(TypeOfAccount));
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
